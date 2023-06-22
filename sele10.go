619a2c69026e6424e97db0f96c730947dbb79d37
godoywipl92 <126252780+godoywipl92@users.noreply.github.com>
2023-06-22 16:01:28 -0300

    solução 10

---
+++ sele10.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var idade int
+
+	fmt.Print("Informe a idade: ")
+	fmt.Scan(&idade)
+
+	if idade <= 9 {
+		fmt.Println("Classificação: Mirim")
+	} else if idade >= 10 && idade <= 13 {
+		fmt.Println("Classificação: Infantil")
+	} else if idade >= 14 && idade <= 17 {
+		fmt.Println("Classificação: Juvenil")
+	} else {
+		fmt.Println("Classificação: Adulto")
+	}
+}
